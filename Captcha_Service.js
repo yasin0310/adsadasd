429326d18ffead92d0f6c6fc869760a1281906a5
yasin0310 <138303344+yasin0310@users.noreply.github.com>
2023-07-04 00:20:09 +0200

    Add files via upload

---
+++ Captcha_Service.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Captcha Service
+// @namespace    mservice
+// @version      1.0
+// @author       Moch
+// @match        https://www.habbo.com/api/public/captcha*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    var socket = new WebSocket("ws://127.0.0.1:8080");
+    socket.onopen = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has("token") && urlParams.get("token") !== "") {
+            socket.send(urlParams.get("token"));
+        }
+    }
+})();
